6698382de71b65410fdbbc252354bb036902eb59
karentsai2016 <karentsai2016@gmail.com>
2016-12-30 11:32:35 +0800

    Using Vue.js to show projects.

---
+++ js/data.js
@@ -0,0 +1,333 @@
+var data={
+    weblist:[
+        {
+            year: 2016,
+            name: "雲智慧Maxnerva",
+            subname: "Maxnerva Website",
+            url: "http://www.maxnerva.com/",
+            url_text: "www.maxnerva.com",
+            description: "雲智慧為一家科技公司，以藍色帶出專業科技感，傳遞專業與科技服務理念。",
+            rwd: true,
+            development: true,
+            design: true,
+            designtype: "Co-Design",
+            img_thumbs: "maxnerva.jpg",
+            img_default: "maxnerva-banner",
+            demo: ["maxnerva-1.jpg","maxnerva-2.jpg"]
+        },
+        {
+            year: 2016,
+            name: "寶貝來交朋友",
+            subname: "https://bbcomes.com/inviteFriends",
+            url: "http://www.maxnerva.com/",
+            url_text: "bbcomes.com/inviteFriends",
+            description: "寶貝來了解新世代父母親的焦慮與無助，我們期望能和新世代的父母一起用最剛好的溫度丶最適宜的態度，以及育兒專家丶醫護等專業建議，幫助您一同解開小小人兒的成長秘密。",
+            rwd: true,
+            development: true,
+            design: true,
+            designtype: "Design",
+            img_thumbs: "bbcomesfriends.jpg",
+            img_default: "bbcomesfriends-banner",
+            demo: ["bbcomesfriends.jpg"]
+        },
+        {
+            year: 2016,
+            name: "木石設計",
+            subname: "WoodStone Website",
+            url: "http://woodstonedesign.github.io",
+            url_text: "woodstonedesign.github.io",
+            description: "木石設計提供網頁設計&開發、App流程與介面設計、使用者經驗服務。融入使用者經驗設計思維，提供友善易懂的操作介面，帶給使用者良好的體驗。",
+            rwd: true,
+            development: true,
+            design: true,
+            designtype: "Design",
+            img_thumbs: "woodstone.jpg",
+            img_default: "woodstone-banner",
+            demo: ["woodstone-1.jpg","woodstone-2.jpg"]
+        },
+        {
+            year: 2016,
+            name: "淨土藝術",
+            subname: "Art of Rangniang",
+            url: "http://www.rangniang.org",
+            url_text: "www.rangniang.org",
+            description: "淨土藝術為讓娘寺的官方網站，讓娘寺全名為「讓娘彭措帖慶林」，意謂「讓娘圓滿大乘洲」，位於青海省玉樹州玉樹市以北60公里處。淨土藝術網站以集資為目標，期望透過募資方式重建因地震而倒塌的建築，祈求儘快實現教學樓及餐飲問題，愛心人士的捐助，讓這些弱小的朋友擁有一個教學樓。",
+            rwd: true,
+            development: true,
+            design: true,
+            designtype: "Co-Design",
+            img_thumbs: "rangniang.jpg",
+            img_default: "rangniang-banner",
+            demo: ["rangniang-1.jpg"]
+        },
+        {
+            year: 2016,
+            name: "北大醫療腦健康",
+            subname: "Brain Health",
+            url: "http://weixin.pkucarenjk.com",
+            url_text: "weixin.pkucarenjk.com",
+            description: "北大醫療腦健康為一兒童量表型測驗網站，透過簡單的量表測驗帶你輕鬆認識腦健康。以水彩風格為設計，讓醫療不再是冰冷感受，充滿活潑、童趣、陪伴的體驗。",
+            rwd: true,
+            development: true,
+            design: false,
+            img_thumbs: "brainhealth.jpg",
+            img_default: "brainhealth-banner",
+            demo: ["brainhealth-1.jpg"]
+        },
+        {
+            year: 2016,
+            name: "寶貝來寵愛媽咪好孕福袋",
+            subname: "Love mommy,love baby.",
+            url: "https://bbcomes.com/mamifun",
+            url_text: "bbcomes.com/mamifun",
+            description: "寶貝來了解新世代父母親的焦慮與無助，我們期望能和新世代的父母一起用最剛好的溫度丶最適宜的態度，以及育兒專家丶醫護等專業建議，幫助您一同解開小小人兒的成長秘密。",
+            rwd: true,
+            development: true,
+            design: true,
+            designtype: "Design",
+            img_thumbs: "mamifun.jpg",
+            img_default: "mamifun-banner",
+            demo: ["mamifun-1.jpg"]
+        },
+        {
+            year: 2016,
+            name: "寶貝來BBcomes",
+            subname: "Hello!Baby.Hello!World.",
+            url: "https://bbcomes.com/",
+            url_text: "bbcomes.com",
+            description: "寶貝來了解新世代父母親的焦慮與無助，我們期望能和新世代的父母一起用最剛好的溫度丶最適宜的態度，以及育兒專家丶醫護等專業建議，幫助您一同解開小小人兒的成長秘密。",
+            rwd: true,
+            development: true,
+            design: true,
+            designtype: "Co-Design",
+            img_thumbs: "bbcomes.jpg",
+            img_default: "bbcomes-banner",
+            demo: ["bbcomes-1.jpg"]
+        },
+        {
+            year: 2015,
+            name: "【夏日暖爸季】我家爸爸超級暖",
+            subname: "Happy Father's Day!",
+            url: "https://bbcomes.com/activity/summerdad",
+            url_text: "summerdad",
+            description: "全天下辛苦的爸爸們，父親節快樂！你家有個願意陪孩子玩到滿身大汗、氣喘吁吁的爸爸嗎？寶貝來父親節活動網站以「我的爸爸是超人，爸爸願意為你做牛做馬」為設計理念。",
+            rwd: true,
+            development: true,
+            design: true,
+            designtype: "Design",
+            img_thumbs: "summerdad.jpg",
+            img_default: "summerdad-banner",
+            demo: ["summerdad-1.jpg"]
+        },
+        {
+            year: 2015,
+            name: "手滑漫畫",
+            subname: "Happy Father's Day!",
+            url: "http://www.comm-ez.com/taptapcomic",
+            url_text: "taptapcomic",
+            description: "玩漫畫。看漫畫。盡在手滑漫畫。此為手滑漫畫App的推廣網頁，以漫畫風格介紹行銷app。",
+            rwd: true,
+            development: true,
+            design: true,
+            designtype: "Design",
+            img_thumbs: "taptapcomic.jpg",
+            img_default: "taptapcomic-banner",
+            demo: ["taptapcomic-1.png","taptapcomic-2.jpg"]
+        },
+        {
+            year: 2015,
+            name: "陽朔旅遊網",
+            subname: "Travel around Yangshuo.",
+            url: "#",
+            url_text: "",
+            description: "旅遊網網頁設計提案",
+            rwd: false,
+            development: false,
+            design: true,
+            designtype: "Design",
+            img_thumbs: "yangshuo.jpg",
+            img_default: "yangshuo-banner",
+            demo: ["yangshuo-1.png"]
+        },
+        {
+            year: 2014,
+            name: "和熹生活",
+            subname: "The Life of Hexi.",
+            url: "#",
+            url_text: "",
+            description: "和熹生活為一個老年人社群網站，專注於分享健康知識、大小活動與個人健康管理。以企業代表色-紅色為設計，傳遞熱情、活躍，期許能有安享幸福晚年的生活。",
+            rwd: false,
+            development: true,
+            design: false,
+            img_thumbs: "hexi.jpg",
+            img_default: "hexi-banner",
+            demo: ["hexi-1.jpg"]
+        },
+        {
+            year: 2014,
+            name: "DoEZ",
+            subname: "EZ Chat.EZ Talk.",
+            url: "http://www.comm-ez.com/doez",
+            url_text: "www.comm-ez.com/doez",
+            description: "都易[Do-EZ]是您生活上最麻吉的好幫手，我們把大家最常用的免費即時通訊，結合了生活上的邀約與邀請，可以讓您一邊聊天，一邊把聊天記錄迅速建立起來成為一個提醒事項，或把聊天內容傳送給對方，邀請對方加入這個活動。",
+            rwd: false,
+            development: true,
+            design: false,
+            img_thumbs: "doez.jpg",
+            img_default: "doez-banner",
+            demo: ["doez-1.jpg"]
+        },
+        {
+            year: 2014,
+            name: "追跡者",
+            subname: "EZ Chat.EZ Talk.",
+            url: "",
+            url_text: "",
+            description: "安全，簡單，追跡。追跡者，您的安全運輸專家。",
+            rwd: false,
+            development: true,
+            design: true,
+            designtype: "Design",
+            img_thumbs: "tracking.jpg",
+            img_default: "tracking-banner",
+            demo: ["tracking-1.jpg"]
+        },
+        {
+            year: 2014,
+            name: "eHR 登入",
+            subname: "Register & Login",
+            url: "",
+            url_text: "",
+            description: "系統後台登入頁。",
+            rwd: false,
+            development: true,
+            design: true,
+            designtype: "Design",
+            img_thumbs: "eHR.jpg",
+            img_default: "eHR-banner",
+            demo: ["eHR-1.png","eHR-2.jpg"]
+        },
+        {
+            year: 2014,
+            name: "WISE Lab",
+            subname: "Wireless Innovative System EM-Applied",
+            url: "http://wisbal.ee.ncku.edu.tw",
+            url_text: "wisbal.ee.ncku.edu.tw",
+            description: "國立成功大學電機系無線創新系統電磁應用實驗室(WISE Lab)網站。",
+            rwd: true,
+            development: true,
+            design: true,
+            designtype: "Design",
+            img_thumbs: "wiselab.jpg",
+            img_default: "wiselab-banner",
+            demo: ["wiselab-1.jpg","wiselab-2.jpg"]
+        }
+        // ,
+        // {
+        //     year: 2014,
+        //     name: "雲列印",
+        //     subname: "Easy Printing.",
+        //     url: "",
+        //     url_text: "",
+        //     description: "雲列印 Easy Printing.",
+        //     rwd: false,
+        //     development: false,
+        //     design: true,
+        //     designtype: "Design",
+        //     img_thumbs: "printing.jpg",
+        //     img_default: "printing-banner",
+        //     demo: ["printing-1.jpg","printing-2.jpg"]
+        // }
+    ],
+    applist:[
+        {
+            year: 2016,
+            name: "SAMENU",
+            subname: "EZ Order.EZ Track.",
+            description: "為幫助顧客減少訂餐、等待餐點時間，SAMENU提供顧客與店家雙向服務，app囊括多家商店，提供顧客快速訂、取餐服務，隨時查看目前叫號狀態。店家可透過app管理訂單進度，讓顧客享有便利與快速的購餐服務。",
+            android: true,
+            ios: true,
+            design: true,
+            img_thumbs: "SAMENU.jpg",
+            img_default: "SAMENU-banner",
+            demo: ["SAMENU-1.jpg"]
+        },
+        {
+            year: 2015,
+            name: "手滑漫畫",
+            subname: "TapTapToon",
+            description: "玩漫畫。看漫畫。盡在手滑漫畫。你有許多不吐不快的話嗎?你有滿腦子好笑的話題想要給朋友知道嗎?用單純的文字來表達太落伍了，用漫畫吧，讓你想說的話更生動活潑有趣。",
+            android: true,
+            ios: true,
+            design: true,
+            img_thumbs: "TapTapToon.jpg",
+            img_default: "taptapcomic-banner",
+            demo: ["TapTapToon-1.png"]
+        },
+        {
+            year: 2014,
+            name: "活動票卷app",
+            subname: "EZ Tickets.",
+            description: "便捷、快速取票與驗票app。",
+            android: true,
+            ios: true,
+            design: true,
+            img_thumbs: "tickets.jpg",
+            img_default: "tickets-banner",
+            demo: ["tickets-1.jpg"]
+        },
+        {
+            year: 2014,
+            name: "HealthCare-健康管理",
+            subname: "Health is wealth.",
+            description: "HealthCare為一個自我健康管理app，擁有健康報告、健康管理、運動紀錄、行事曆、健康資訊、預約健檢六大功能。",
+            android: true,
+            ios: true,
+            design: true,
+            img_thumbs: "HealthCare.jpg",
+            img_default: "HealthCare-banner",
+            demo: ["HealthCare-1.jpg","HealthCare-2.png","HealthCare-3.jpg"]
+        }
+    ],
+    uilist:[
+        {
+            name: "Leaderboard",
+            img_default: "day17"
+        },
+        {
+            name: "Profile",
+            img_default: "day4"
+        },
+        {
+            name: "Music Player",
+            img_default: "day6"
+        },
+        {
+            name: "404 Page",
+            img_default: "day8"
+        },
+        {
+            name: "Email check",
+            img_default: "day15"
+        },
+        {
+            name: "Sport Chart",
+            img_default: "day16"
+        },
+        {
+            name: "Night Mode",
+            img_default: "day13"
+        },
+        {
+            name: "Shopping",
+            img_default: "day10"
+        }
+    ]
+};
+
+
+
+var vm = new Vue({
+    el: "#allprojects",
+    data: data
+});
